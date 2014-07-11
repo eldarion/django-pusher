26d22a5c8c328dd051dd50a6b846a3d09582d869
Patrick Altman <paltman@gmail.com>
2014-07-11 19:00:56 -0400

    Another lint error

--- django_pusher/urls.py
+++ django_pusher/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns("",
-    url("^auth/$", "django_pusher.views.pusher_auth", name="pusher_auth"),
+urlpatterns = patterns(
+    "",
+    url(r"^auth/$", "django_pusher.views.pusher_auth", name="pusher_auth"),
 )
